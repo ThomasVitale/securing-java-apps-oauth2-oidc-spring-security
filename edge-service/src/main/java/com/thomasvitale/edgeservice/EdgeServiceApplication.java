df0684fbc7909a8c2b1c08f02c443f6e10c97bc7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-10-12 19:57:12 +0200

    Add authentication with OIDC

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -2,6 +2,10 @@ package com.thomasvitale.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,6 +15,14 @@ public class EdgeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
+    }
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+            .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+            .oauth2Login(Customizer.withDefaults())
+            .build();
     }
 
 }
